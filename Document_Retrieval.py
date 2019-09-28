0bb5b19163effaeb97d9dfa2c2ab2a287ebd5b60
pranjali2018201029 <42588488+pranjali2018201029@users.noreply.github.com>
2019-09-29 01:05:03 +0530

    Document Retrieval Skeleton

---
+++ Document_Retrieval.py
@@ -0,0 +1,14 @@
+## Create Index for Document Abstract
+
+
+## Get Posting Lists for query words and
+## Tranform Posting lists of words in query abstract into 
+## Vector representationsn (TF_IDF score) of document abstracts
+
+
+## Calculate TF-IDF score of Query Abstract, Vector Representation of Query Abstract
+
+## Cosine Similarity fuction : I/P - Two vectors, O/P - Similarity value
+
+
+## Create Similarity Matrix
